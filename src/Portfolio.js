435df1bed2d1c1b77d24c8feb43f8b6ed1db95c2
Isreality <isrealitycreations@gmail.com>
2024-09-19 18:43:41 +0100

    image

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -111,29 +111,20 @@ const Portfolio = () => {
                                     </Zoom>
                                 </div>
 
-                                <div className="flex sm:flex-col md:flex-row gap-10">
+                                <div className="flex sm:flex-col md:flex-row gap-2">
                                     <Zoom clear>
                                         <Card header="Trixmed" btn="Live" image={trixmed} text="Healthcare | Transportation " link={'https://www.trixmedtransportation.com/'}/><br/>
                                         <Card header="Livestock" btn="In progress" image={livestock} text="Animal | Health " link={'#'}/><br/>  
                                     </Zoom>
-                                </div>
+                                </div><br/>
 
-                                <div className="flex sm:flex-col md:flex-row gap-10">
+                                <div className="flex sm:flex-col md:flex-row gap-2">
                                     <Zoom clear>
-                                        <Card header="Trixmed" btn="Live" image={trixmed} text="Healthcare | Transportation " link={'https://www.trixmedtransportation.com/'}/><br/>
-                                        <Card header="Livestock" btn="In progress" image={livestock} text="Animal | Health " link={'#'}/><br/>  
+                                    <Card header="Goodness Gym" btn="In progress" image={goodness} text="Fitness" link={'#'}/><br/>
+                                    <Card header="Foodies" btn="Case Study" image={foodie} text="Food" link={'https://www.behance.net/gallery/155884893/Foodie-UIUX-Case-Study'}/> 
                                     </Zoom>
-                                </div>
-
-                                    {/* <Zoom clear>
-                                        <div>
-                                            <Card header="Goodness Gym" btn="In progress" image={goodness} text="Fitness" link={'#'}/><br/>
-                                            <Card header="Foodies" btn="Case Study" image={foodie} text="Food" link={'https://www.behance.net/gallery/155884893/Foodie-UIUX-Case-Study'}/>
-                                        </div>
-                                    </Zoom> */}       
-                                    
-                                <br/><br/>
-                                
+                                </div><br/><br/>     
+  
                                 <Bounce clear>
                                     <Link to='https://www.behance.net/agbajeisrael' target="_blank" className="animate-bounce bg-primary py-4 px-16 justify-items-start ml-0 text-white rounded-full hover:bg-white hover:text-primary hover:border-2 border-primary">View More</Link> 
                                 </Bounce> 
