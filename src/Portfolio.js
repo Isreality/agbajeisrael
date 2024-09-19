7d5176a772f65aa1eaf348340fa29b29445caa69
Isreality <isrealitycreations@gmail.com>
2024-09-19 21:19:01 +0100

    row

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -115,7 +115,7 @@ const Portfolio = () => {
                                 </div>
 
                                 {/* <div className="flex sm:flex-col md:flex-row gap-5"> */}
-                                <div className="grid sm:grid-cols-1 lg:grid-rows-2 gap-10">
+                                <div className="grid sm:grid-cols-1 lg:grid-rows-1 gap-10">
                                     <Zoom clear>
                                         <Card header="Trixmed" btn="Live" image={trixmed} text="Healthcare | Transport " link={'https://www.trixmedtransportation.com/'}/><br/>
                                         <Card header="Livestock" btn="In progress" image={livestock} text="Animal | Health " link={'#'}/><br/>  
