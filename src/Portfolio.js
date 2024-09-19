cd3007df4c5f36bb704ba39d1457b29770d877e0
Isreality <isrealitycreations@gmail.com>
2024-09-19 18:57:44 +0100

    update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -116,17 +116,16 @@ const Portfolio = () => {
                                         <Card header="Trixmed" btn="Live" image={trixmed} text="Healthcare | Transportation " link={'https://www.trixmedtransportation.com/'}/><br/>
                                         <Card header="Livestock" btn="In progress" image={livestock} text="Animal | Health " link={'#'}/><br/>  
                                     </Zoom>
-                                </div><br/>
+                                </div><br/><br/> 
 
-                                <div className="flex sm:flex-col md:flex-row gap-5">
+                                {/* <div className="flex sm:flex-col md:flex-row gap-5">
                                     <Zoom clear>
                                         <div>
                                             <Card header="Goodness Gym" btn="In progress" image={goodness} text="Fitness" link={'#'}/><br/>
                                         </div>
                                         <div className=""></div>
-                                    {/* <Card header="Foodies" btn="Case Study" image={foodie} text="Food" link={'https://www.behance.net/gallery/155884893/Foodie-UIUX-Case-Study'}/>  */}
                                     </Zoom>
-                                </div><br/><br/>     
+                                </div><br/><br/>      */}
   
                                 <Bounce clear>
                                     <Link to='https://www.behance.net/agbajeisrael' target="_blank" className="animate-bounce bg-primary py-4 px-16 justify-items-start ml-0 text-white rounded-full hover:bg-white hover:text-primary hover:border-2 border-primary">View More</Link> 
